aba8f15ff731b0cb9aac9a61cb8ec55a42512adf
Cleo Le Tron <ale-tron@cbr14s3.42barcelona.com>
2023-12-16 18:23:20 +0100

    norminette

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,18 +6,17 @@
 /*   By: ale-tron <ale-tron@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 13:21:27 by ale-tron          #+#    #+#             */
-/*   Updated: 2023/12/16 17:57:36 by ale-tron         ###   ########.fr       */
+/*   Updated: 2023/12/16 18:22:58 by ale-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/push_swap.h"
 
 int	main(int argc, char **argv)
 {
-	t_list	*stack_a;
+	t_list	*node_a;
 
 	if (argc == 1)
 		return (0);
-//	printf("%d", argc);
 	if (ft_check_error(argc, argv))
 		return (ft_putstr_fd("Error\n", 1), 0);
 	return (0);
